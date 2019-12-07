52712d29451b21f469e3c5f51dc12001e05b8dce
Anton <parapet058@gmail.com>
2019-12-07 18:56:54 +0300

    Init commit
    created empty project

---
+++ src/test/java/ru/priamosudov/hotelme/HotelmeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.priamosudov.hotelme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
